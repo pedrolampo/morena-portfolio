da555e7636e2844713d68e57a56f3f81417a1663
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-05-10 14:53:18 -0300

    ui changes

--- src/components/Productos/ProductRow.js
+++ src/components/Productos/ProductRow.js
@@ -11,24 +11,6 @@ export default function ProductRow({ title }) {
           />
           <h3 className="product-title">{title}</h3>
         </div>
-        <div className="product-item">
-          <img
-            src="https://dglb26w8rx2ld.cloudfront.net/000_clients/3033794/page/w400-w21292tLCywAMFtN.jpg"
-            alt="Product Category"
-          />
-        </div>
-        <div className="product-item">
-          <img
-            src="https://dglb26w8rx2ld.cloudfront.net/000_clients/3033794/page/w400-w21292tLCywAMFtN.jpg"
-            alt="Product Category"
-          />
-        </div>
-        <div className="product-item">
-          <img
-            src="https://dglb26w8rx2ld.cloudfront.net/000_clients/3033794/page/w400-w21292tLCywAMFtN.jpg"
-            alt="Product Category"
-          />
-        </div>
       </div>
     </>
   );
--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -8,12 +8,14 @@ export default function Productos() {
     <div className="products">
       <h2>FOTO PRODUCTO</h2>
 
-      <ProductRow title="VICENTA AROMAS" />
-      <ProductRow title="PALOTES SIERRAS APART" />
-      <ProductRow title="TEXTURA: DISEÑO EN TELAR" />
-      <ProductRow title="MS PRINT" />
-      <ProductRow title="IRACY ALMACÉN NATURAL" />
-      <ProductRow title="CONCEPTO SHOWROOM" />
+      <div className="products-list">
+        <ProductRow title="VICENTA AROMAS" />
+        <ProductRow title="PALOTES SIERRAS APART" />
+        <ProductRow title="TEXTURA: DISEÑO EN TELAR" />
+        <ProductRow title="MS PRINT" />
+        <ProductRow title="IRACY ALMACÉN NATURAL" />
+        <ProductRow title="CONCEPTO SHOWROOM" />
+      </div>
     </div>
   );
 }
