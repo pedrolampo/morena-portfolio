f6de2dbc5abbabd3fac8988760c85cd831e96b25
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-06-14 20:27:41 -0300

    added contact form + ui fixes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import './Contact.css';
+
 export default function Contact() {
-  return <div>Pegame un tubaso al fono</div>;
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSe7ybq_kBFLH2jdk7xDJPJqKGqOBXqLCCbigwgo06vETM_YWw/viewform?embedded=true"
+        width="640"
+        height="775"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="Contact Form"
+        className="contact-form"
+      >
+        Cargando…
+      </iframe>
+    </div>
+  );
 }
